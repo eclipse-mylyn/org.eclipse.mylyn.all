d38fbe55581260b9c7975b7ac95b2e0a3ce4bd1f
Steffen Pingel <steffen.pingel@tasktop.com>
2011-07-11 20:30:32 +0200

    Update for integration build.
    === org.eclipse.mylyn ===
    
    commit 1fcbabdc770991c42adc0c1a6b11eeac89da3979
    Author: Steffen Pingel <steffen.pingel@tasktop.com>
    Date:   Mon Jul 11 20:26:44 2011 +0200
    
        add Maven repository for Mylyn artifacts
    
        bug 329561: [releng] migrate Mylyn projects to Git
        https://bugs.eclipse.org/bugs/show_bug.cgi?id=329561
    
    === org.eclipse.mylyn.builds ===
    
    === org.eclipse.mylyn.commons ===
    
    === org.eclipse.mylyn.context ===
    
    === org.eclipse.mylyn.docs ===
    
    === org.eclipse.mylyn.reviews ===
    
    === org.eclipse.mylyn.tasks ===
    
    === org.eclipse.mylyn.versions ===

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -f .commits.log
+echo -e Update for integration build\\n\\n > .commits.log
 git submodule foreach 'git pull && echo \\n=== $path ===\\n >> ../.commits.log && git log $sha1.. >> ../.commits.log'
 git commit -a -t .commits.log
