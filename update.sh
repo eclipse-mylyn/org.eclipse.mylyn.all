f7073645069c22baffd5fa16741aa27487ad2733
Steffen Pingel <steffen.pingel@tasktop.com>
2011-08-18 00:56:18 +0200

    Update for integration build (1 bug)     RESOLVED - bug 353316: discovery fails on 3.7 behind NTLM proxies

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export LOCAL=$1
+
 echo > .commits.log
-git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
+git submodule foreach '(test "$LOCAL" = "-local" || git pull) && git log $sha1.. >> ../.commits.log'
 
 echo Update for integration build > .commits.msg
 cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
