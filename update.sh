45c207025d268877506a36c854df33013461fd9e
Steffen Pingel <steffen.pingel@tasktop.com>
2011-08-17 16:00:10 +0200

    Update for integration build:     336592: [patch] XslfoDocumentBuilder should be able to generate a bookmark tree     336683: XslFoDocumentBuilder should allow for basic page styling     ASSIGNED - bug 310059: [patch][api][framework] provide 'update     ASSIGNED - bug 343544: add SDK feature and site for Commons, Context,     ASSIGNED - bug 347365: [UI] address UI review findings     ASSIGNED - bug 348125: remove     ASSIGNED - bug 349771: create UI for support adding private comments     ASSIGNED - bug 349828: Update R4E Documentation     ASSIGNED - bug 349924: [patch][api] provide extension point for task     ASSIGNED - bug 350633: Implement button in R4E preferences to validate     bug 302291: [Textile] text containing CDATA produces invalid HTML     bug 321538: strikethrough rendering fails when a hyphenated word is present     bug 339956: reduce redundancy in build and simplify target management     bug 346161: wikitext standalone bundle is not being updated with weekly builds     bug 346672: [MediaWiki] Can't handle comments in wiki source     bug 349598: [patch] PersonProposalProvider should set currentUser when     bug 352228: Broken error message while searching     bug 354077: ConfluenceLanguage does not use internalLinkPattern     bug 354570: [patch] Show content-type icons in patch set section     bug 354753: [patch] decorate user's comments in Gerrit reviews     bug 354879: disable javadoc generation on workspace build     NEW - bug 336430: separate Context framework from Tasks     NEW - bug 337980: [patch] AbstractRepositoryQueryPage() constructor     NEW - bug 348404: Build editor shows wrong number of tests     NEW - bug 348619: [api] Support the ability to set the filter text on     NEW - bug 348901: [report] Define the report Layout     NEW - bug 350561: [editor] text appears backwards when using large text     NEW - bug 352584: create nightly repositories for sub-projects     NEW - bug 352915: extract test support classes into sdk bundles     NEW - bug 352916: [patch] Show artifact hierarchy in build editor     NEW - bug 352918: [patch] Attribute editors generate duplicate change     NEW - bug 352919: [patch] Password field text set to user name     NEW - bug 352933: [api] extract task activity monitoring from     NEW - bug 353222: ensure that Java tests are executed     NEW - bug 353222: org.eclipse.mylyn.java.tests are not getting executed     NEW - bug 353367: attach context should be disabled when there is no     NEW - bug 354422: manage participation in Juno     REOPENED - bug 339956: reduce redundancy in build and simplify target     REOPENED - bug 349633: BugzillaRepositoryConnector.getTaskData() fails     REOPENED - bug 350334: [patch] add preference for showing test failures     RESOLVED - bug 343544: add SDK feature and site for Commons, Context,     RESOLVED - bug 347505: [UI] Mail notification enhancements     RESOLVED - bug 347913: Review property view column not needed     RESOLVED - bug 348083: [UI] Notifications enhacements     RESOLVED - bug 350610: Run long running operations in separate thread     RESOLVED - bug 352584: create nightly repositories for sub-projects

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo -e Update for integration build\\n\\n > .commits.log
-git submodule foreach 'git pull && echo \\n=== $path ===\\n >> ../.commits.log && git log $sha1.. >> ../.commits.log'
-git commit -a -t .commits.log
+
+echo > .commits.log
+git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
+
+echo Update for integration build > .commits.msg
+cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
+
+git commit -a -t .commits.msg
