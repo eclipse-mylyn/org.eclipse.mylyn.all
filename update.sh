7eb158c6bb6fc6dac8b0834cd3d7f2a1bbd9e3dc
Steffen Pingel <steffen.pingel@tasktop.com>
2011-09-04 17:11:41 +0200

    Update for integration build.

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo > .commits.log
-git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
+if [ "$1" == "-np" ]; then
+ git submodule foreach 'git log $sha1.. >> ../.commits.log'
+else
+ git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
+fi
 
 echo Update for integration build > .commits.msg
 cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
