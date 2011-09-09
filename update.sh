4525bd1c0c73d6b46106649224b57bc7a0d08526
Steffen Pingel <steffen.pingel@tasktop.com>
2011-09-09 21:10:12 +0200

    Update for integration build (32 changes):  347931 [gerrit] Add free-form query  352647  [gerrit] Fall back to task repository url for fetching patchset  352647 [gerrit] Fix config handling  352647 Refactored GerritConfig retrieval  353869 ignore GerritForge specific AuthType "TEAMFORGE"  address OpenID authentication compatibility problem with Eclipse 3.5  assertion error when retrieving task data  bug 310059: [api][framework] provide 'update configuration'  bug 341434: support openid for login to gerrit  bug 347522: [UI] Add new Filter: Hide Completed Reviews  bug 355977: Adding review items from SVN is really slow  bug 356253: Review obsolete dependencies - Subclipse UI  bug 356334: Combo Boxes handling state transition shall not  bug 356344: Review Navigator tool bar remains disabled  bug 356642: Content not always included when sending email  bug 356855: Formal Reviews - Unable to add Decision Participants  bug 356958: [Subclipse SVN connector]: Null pointer exception due  Fix Gerrit configuration refresh behind proxy server  Fix Gerrit tests launch configuration  fix query page font and layout  [gerrit] Add query 'My watched changes'  [gerrit] Enhance launch configuration  [gerrit] Fix task completion state handling  make summary read-only  NPE when opening review  NPE when publishing comments  provide support for Gerrit 2.2  support authentication by account id in 'development only mode'  support openid for login to gerrit  update commit template  Update copyright header  Update launch configuration for Gerrit connector tests

--- update.sh
+++ update.sh
@@ -7,7 +7,10 @@ else
  git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
 fi
 
-echo Update for integration build > .commits.msg
-cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
+COUNT=$(grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*//' -e 's/^NEW - //' -e 's/^ASSIGNED - //' -e 's/^RESOLVED - //' | sort -u | wc -l)
+
+echo Update for integration build \($COUNT changes\) > .commits.msg
+grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*/ /' -e 's/^ NEW - / /' -e 's/^ ASSIGNED - / /' -e 's/^ RESOLVED - / /' | sort -u >> .commits.msg
+#cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
 
 git commit -a -t .commits.msg
