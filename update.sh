0d0cfca7011a354cf5b3184f9e5550fff00f6d81
Steffen Pingel <steffen.pingel@tasktop.com>
2011-09-11 12:57:54 +0200

    Update for integration build (11 changes):  bug 356253: cvs core remove dependency no longer needed  bug 356348: Properties "ADD" options are not friendly  bug 357218: Adding subclipse feature to versions-site  bug 357218: [Subclipse] Add feature bundle  bug 357294: [api] provide API for converting content to wiki markup  bug 357310: [windows] fix OpenID support on IE  enhance changesets with author and date/time  react on hyperlinks for gerrit changes  submit button is not showing for Gerrit 2.2  support hyperlinking Gerrit reviews from a Change-Id  [windows] fix OpenID support on IE

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ fi
 COUNT=$(grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*//' -e 's/^NEW - //' -e 's/^ASSIGNED - //' -e 's/^RESOLVED - //' | sort -u | wc -l)
 
 echo Update for integration build \($COUNT changes\) > .commits.msg
-grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*/ /' -e 's/^NEW - //' -e 's/^ASSIGNED - //' -e 's/^RESOLVED - //' | sort -u >> .commits.msg
+grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*/ /' -e 's/^ NEW - / /' -e 's/^ ASSIGNED - / /' -e 's/^ RESOLVED - / /' | sort -u >> .commits.msg
 #cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
 
 git commit -a -t .commits.msg
