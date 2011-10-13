08259bcad03144ec4415eaa546cd90e9f2c84bea
Steffen Pingel <steffen.pingel@tasktop.com>
2011-10-13 20:17:31 +0200

    Update for integration build  (6 changes)  bug 359300: submitting new bugs to Bugzilla 4.0 results in  bug 360743: NPE in TaskEditorSummaryPart when there is no Summary  bug 360873: update versions for 3.6.3  update target definition for Indigo SR1

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-export LOCAL=$1
-
 echo > .commits.log
-git submodule foreach '(test "$LOCAL" = "-local" || git pull) && git log $sha1.. >> ../.commits.log'
+if [ "$1" == "-np" ]; then
+ git submodule foreach 'git log $sha1.. >> ../.commits.log'
+else
+ git submodule foreach 'git pull && git log $sha1.. >> ../.commits.log'
+fi
 
-echo Update for integration build > .commits.msg
-cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
+COUNT=$(grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*//' -e 's/^NEW - //' -e 's/^ASSIGNED - //' -e 's/^RESOLVED - //' | sort -u | wc -l)
+
+echo Update for integration build \($COUNT changes\) > .commits.msg
+grep Date: .commits.log -A 2 | sed -e '/^$/d' -e '/--/d' -e '/Date: /d' -e 's/^[ \t]*/ /' -e 's/^ NEW - / /' -e 's/^ ASSIGNED - / /' -e 's/^ RESOLVED - / /' | sort -u >> .commits.msg
+#cat .commits.log | grep -E '^\s*(.*bug )?[0-9]+:' | sort -u >> .commits.msg
 
 git commit -a -t .commits.msg
